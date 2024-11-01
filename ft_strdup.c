c3146cfbdfd05e00b86731746dcd29b77a5b64e0
iperdids <ipersids@student.hive.fi>
2024-11-01 18:03:11 +0200

    Update ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 19:13:56 by ipersids          #+#    #+#             */
-/*   Updated: 2024/10/30 14:46:14 by ipersids         ###   ########.fr       */
+/*   Updated: 2024/11/01 18:01:10 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,22 @@
  * Function shall return a pointer to a new string, which is a duplicate 
  * of the string pointed to by str. The returned pointer can be passed 
  * to free(). A null pointer is returned if storage space available 
- * is insufficient (memory allocation fail) or str pointed NULL.
+ * is insufficient (memory allocation fail).
  * 
  * Notes: this function doesn't set `errno` to indicate the error.
  * 
  * @param str pointer to the null-terminated byte string to duplicate
  * @return char* pointer to a new string on success. 
  * Otherwise, it shall return a null pointer
+ * 
+ * @note If the input `str` is a NULL pointer, the function will cause 
+ * 		 a segfault like the original one.
  */
 char	*ft_strdup(const char *str)
 {
 	size_t	len;
 	char	*copy;
 
-	if (!str)
-		return (0);
 	len = ft_strlen(str);
 	copy = (char *)malloc(len + 1);
 	if (!copy)
