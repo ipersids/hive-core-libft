a98ff8f1b32ba15226baede9fd165ccb34260271
iperdids <ipersids@student.hive.fi>
2024-11-01 18:09:35 +0200

    Update ft_strlen.c

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: ipersids <ipersids@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/28 18:51:21 by ipersids          #+#    #+#             */
-/*   Updated: 2024/10/30 14:43:09 by ipersids         ###   ########.fr       */
+/*   Updated: 2024/11/01 18:07:19 by ipersids         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,12 @@
 size_t	ft_strlen(const char *s)
 {
 	size_t	len;
-	size_t	i;
 
 	len = 0;
-	i = 0;
-	while (s[i] != '\0')
+	while (*s != '\0')
 	{
 		len++;
-		i++;
+		s++;
 	}
 	return (len);
 }
